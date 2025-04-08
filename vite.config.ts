7aebad654ed60261b895a8d3090597e29c2538d5
oskarrough <oskar@rough.dk>
2025-04-08 17:57:39 +0200

    Set build target to esnext

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,8 @@ import { defineConfig } from "vite";
 import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig({
-	plugins: [cloudflare()]
+	plugins: [cloudflare()],
+	build: {
+		target: 'esnext'
+	}
 })
