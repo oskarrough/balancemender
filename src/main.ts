8bc09141424ddd1c2a72d11835bb91bb3ec34d5d
oskarrough <oskar@rough.dk>
2025-04-10 18:31:18 +0200

    Refactor to <web-healer>

--- src/main.ts
+++ src/main.ts
@@ -1,44 +1,9 @@
-import {render} from './utils'
-import {GameLoop} from './nodes/game-loop'
-import {Menu} from './components/menu'
-import gsap from 'gsap'
-import {DevConsole} from './components/dev-console'
-import {InputManager} from './input-manager'
-import './components/dev-console'
 import './components/floating-view.js'
+import './components/dev-console'
 import './components/combat-log-viewer.js'
 import './components/color-palette.js'
 import './components/balance-monitor.js'
+import './components/2d.js'
+import './components/web-healer.js'
 import './style.css'
 
-/**
- * Main entry point for the game.
- * Renders two components, the splash "menu" and the "game" itself.
- */
-function main() {
-	const game = new GameLoop()
-	game.element = document.querySelector('#webhealer')
-	game.render()
-	setupDevTools(game)
-	new InputManager(game)
-	// @ts-ignore
-	window.webhealer = game
-	render(document.querySelector('#menu')!, () => Menu(game))
-	const urlParams = new URLSearchParams(window.location.search)
-	const muted = urlParams.has('muted')
-	if (muted) game.muted = true
-	gsap.to('.Frame', {opacity: 1, duration: 1})
-	// gsap.set('.Frame', {opacity: 1})
-}
-
-function setupDevTools(game: GameLoop) {
-	const devConsole = document.querySelector('dev-console') as DevConsole
-	if (!devConsole) {
-		console.error('Dev console element not found in the DOM')
-		return
-	}
-	devConsole.init(game)
-	game.console = devConsole
-}
-
-main()
