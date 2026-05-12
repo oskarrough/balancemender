c4995ee309522a52e946d8307c640667dfedf8ba
oskarrough <oskar@rough.dk>
2026-05-12 23:56:18 +0200

    Extract Encounter from GameLoop

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
 import {defineConfig} from 'vite-plus'
 
 export default defineConfig({
+	lint: {
+		options: {
+			typeAware: true,
+			typeCheck: true,
+		},
+	},
 	fmt: {
 		bracketSpacing: false,
 		printWidth: 120,
